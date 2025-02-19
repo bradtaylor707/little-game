bfe3e6c64e5e8ba286748118b897d01ee2475642
bradtaylor707 <bradtaylor707@gmail.com>
2025-02-18 18:00:21 -0800

    some progress

--- src/app.tsx
+++ src/app.tsx
@@ -1,51 +1,95 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { uniqueId } from "lodash-es";
+import { BrowserRouter } from "react-router";
+import { useEffect, useState } from "react";
 import "./app.scss";
-import { BrowserRouter, useSearchParams } from "react-router";
 
-function LittleGuy() {
-  const [searchParams, setSearchParams] = useSearchParams({ count: (0).toString() });
-  const count = +(searchParams.get("count") ?? 0);
+const GRID_SIZE = 10;
+
+enum Status {
+  Open,
+  Occupied,
+}
+
+class Square {
+  id = uniqueId("square");
+
+  constructor(public status: Status = Status.Open) {}
+}
+
+function LittleGame() {
+  const [grid, _setGrid] = useState(() => {
+    const _grid: Array<Square[]> = [];
+
+    for (let i = 0; i < GRID_SIZE; i++) {
+      _grid[i] = _grid[i] || [];
+
+      for (let j = 0; j < GRID_SIZE; j++) {
+        _grid[i][j] = new Square();
+      }
+    }
+
+    return _grid;
+  });
+
+  const [position, setPosition] = useState<[number, number]>([0, 0]);
+
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      // todo: should inform some manager to move so that we can look at the adjacent spaces easier
+
+      switch (event.code) {
+        case "ArrowUp":
+          debugger;
+          break;
+        case "ArrowDown":
+          break;
+        case "ArrowLeft":
+          break;
+        case "ArrowRight":
+          break;
+        default:
+          break;
+      }
+    };
+
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={"little-game"}>
+      <div className={"debug-container"}>
+        <div>Grid Size: {GRID_SIZE}</div>
       </div>
-      <h1>Little Game</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            const nextCount = count + 1;
-
-            setSearchParams((prev) => {
-              prev.set("count", nextCount.toString());
-
-              return prev;
-            });
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className={"game-container"}>
+        <div className={"game"}>
+          {grid.map((squares, rowIndex) => {
+            return (
+              <div key={rowIndex} className={"row"}>
+                {squares.map((square, colIndex) => {
+                  return (
+                    <div key={square.id || colIndex} className={"square"}>
+                      Square: {square.status}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
 
 function App() {
   return (
     <BrowserRouter>
-      <main className="app">
-        <LittleGuy />
+      <main className={"app"}>
+        <LittleGame />
       </main>
     </BrowserRouter>
   );
